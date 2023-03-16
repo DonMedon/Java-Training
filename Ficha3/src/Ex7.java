e5dbc692b1932910251cc9ab9ed199aa574a4c14
DonMedon <bernardomedon@gmail.com>
2023-03-16 16:56:46 +0000

    trabalho em sala

--- Ficha3/src/Ex7.java
+++ Ficha3/src/Ex7.java
@@ -8,19 +8,24 @@ public class Ex7 {
         //Faça um programa que vai pedindo números ao utilizador até que este introduza o número -1.
         //O computador deve dizer a média dos números introduzidos (excluindo o -1).
 
-        int n, media;
+        int n, counter = 0, soma = 0;
+        double media;
 
         System.out.println("Introduza um número");
         n = input.nextInt();
 
+
         while (n != -1) {
 
+            soma = soma + n;
+            counter++;
             System.out.println("Introduza um número");
             n = input.nextInt();
 
+
         }
 
-
-
+        media = soma / counter;
+        System.out.println(media);
     }
 }
