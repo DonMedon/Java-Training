b2ae0bbc567b25cacc4a9f69a76a497adcf38995
DonMedon <bernardomedon@gmail.com>
2023-03-17 13:00:52 +0000

    add ficha4

---
+++ Ficha4/src/Ex1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void Ex1(String[] args) {
+        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+
+        //Implemente  um  programa  que  peça  ao  utilizador  dois  valores
+        //e  um  carácter  representando  uma das  quatro  operações aritméticas (+, -,  *,  /).
+        //Apresente  o  resultado  de  aplicar a operação correspondente  aos  valores.
+        //No  final, deverá  perguntar  ao  utilizador  se  deseja  repetir,  permitindo-lhe efetuar novos cálculos.
+        //Exemplo:Deseja continuar? (introduza s/n).
+
+
+        int num1, num2 = input.nextInt();
+        String opcao;
+
+        System.out.println("Insira dois valores");
+        num1 = input.nextInt();
+        num2 = input.nextInt();
+
+        
+
+
+    }
+}
